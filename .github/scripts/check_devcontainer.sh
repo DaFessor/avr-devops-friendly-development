28ac4c9d056cde0417b2da5ee19c52527ea75df4
Steen Krøyer <stek@via.dk>
2025-01-24 11:41:37 +0100

    Collect deployables (#5)
    * Added test and coverage report generation
    
    * Added raw test report generation
    
    * Do not auto build on project open
    
    * Remove -Werror flag from compilation in Makefiles
    
    * Fix image removal command in check_devcontainer script (use tag)
    
    * Test of image delete - does nothing
    
    * More image delete testing
    
    * Made script delete image via github api
    
    * More image delete stuff
    
    * Use api to delete all images
    
    * More image delete stuff
    
    * More image delete
    
    * Delete old images after creation of a new one
    
    * Dummy dockerfile update to trigger new image
    
    * Added project page
    
    ---------
    
    Co-authored-by: Steen Krøyer <siggy.power@gmail.com>
    Co-authored-by: Steen Krøyer <“stek@via.dk”>

--- .github/scripts/check_devcontainer.sh
+++ .github/scripts/check_devcontainer.sh
@@ -20,7 +20,8 @@ echo "IMAGE_PATH=${IMAGE_PATH}"
 REPO_NAME_URL_ENC=$(echo "${REPO_NAME}" | cut -f2 -d/)
 IMAGE_NAME=$(echo "${IMAGE_PATH}" | cut -f4 -d/ | cut -f1 -d:)
 REPO_NAME_URL_ENC="${REPO_NAME_URL_ENC}%2F${IMAGE_NAME}"
-IMAGE_PATH_NO_TAG=$(echo "${IMAGE_PATH}" | cut -f1 -d:)
+
+echo "IMAGE_NAME=${IMAGE_NAME}"
 
 # Change to .devcontainer folder and calculate Dockerfile hash
 cd "${GITHUB_WORKSPACE}"/.devcontainer || exit 1
@@ -29,20 +30,35 @@ echo "Docker hash: ${DOCKER_HASH}"
 
 # Get info on any existing image and try to match hashes
 echo "Rest API url: https://api.github.com/user/packages/container/${REPO_NAME_URL_ENC}/versions"
-IMG_HITS=$(curl -s -L -H "Accept: application/vnd.github+json" \
+
+RESPONSE=$(curl -s -L -H "Accept: application/vnd.github+json" \
                    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
                    -H "X-GitHub-Api-Version: 2022-11-28" \
-                   "https://api.github.com/user/packages/container/${REPO_NAME_URL_ENC}/versions" | grep -c "${DOCKER_HASH}")
-echo "Number of hash matches: ${IMG_HITS}"
+                   "https://api.github.com/user/packages/container/${REPO_NAME_URL_ENC}/versions")
+
+IMG_COUNT=$(echo "${RESPONSE}" | grep -c name)
+IMG_MATCHES=$(echo "${RESPONSE}" | grep -c "${DOCKER_HASH}")
+IMG_IDS=$(echo "${RESPONSE}" | grep \"id\" | cut -f2 -d: | cut -f1 -d,)
+
+echo "Number of existing images: ${IMG_COUNT}"
+echo "Number of matching images: ${IMG_MATCHES}"
+echo "Image IDs:\n ${IMG_IDS}\n"
 
 # If hashes don't (or don exist), build and push image
-if [ "${IMG_HITS}" -lt  1 ]; then
-    echo "Rebuilding image, deleting any old stuff ...."
-    docker image rm "${IMAGE_PATH_NO_TAG}" || true
-    echo "Doing the actual build ...."
+if [ "${IMG_MATCHES}" -lt  1 ]; then
+    echo "No image matches, rebuilding image and deleting any old stuff ...."
+    echo "Building image ...."
     docker build -t "${IMAGE_PATH}" --label org.opencontainers.image.description="${DOCKER_HASH}" .
     echo "Pushing new image to ${IMAGE_PATH} ...."
     docker push "${IMAGE_PATH}"
+    echo "Deleting old images ...."
+    for image in ${IMG_IDS}; do
+        echo "Deleting image ${image} ...."
+        curl -s -L -X DELETE -H "Accept: application/vnd.github+json" \
+            -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+            -H "X-GitHub-Api-Version: 2022-11-28" \
+            "https://api.github.com/user/packages/container/${REPO_NAME_URL_ENC}/versions/${image}" || true
+    done
 else
     echo "Image ${IMAGE_PATH} up to date, nothing to do ...."
 fi
