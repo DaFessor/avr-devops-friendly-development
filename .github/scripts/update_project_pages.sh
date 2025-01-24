cab848c3089b071fc09471741d30447f8b4ddb23
Steen Krøyer <stek@via.dk>
2025-01-24 11:50:47 +0000

    More debug info

--- .github/scripts/update_project_pages.sh
+++ .github/scripts/update_project_pages.sh
@@ -15,15 +15,25 @@ PAGES_URL="https://${REPO_OWNER}.github.io/${REPO_NAME}/"
 WORKDIR=$(pwd)
 echo "Working directory: ${WORKDIR}"
 
-echo "Update project pages variables:"
+echo "Project pages variables:"
 echo "REPOSITORY = ${REPOSITORY}"
 echo "REPO_OWNER = ${REPO_OWNER}"
 echo "REPO_NAME = ${REPO_NAME}"
 echo "PAGES_URL = ${PAGES_URL}"
 
+echo "List files in the current directory"
+ls -al .
+echo "List files in the build directory"
+ls -al ./build
+echo "List files in project_pages directory"
+ls -al ./proj_pages
+echo "List files in project_pages/firmware directory"
+ls -al ./proj_pages/firmware
+
+
 # Update firmware.zip
-rm -f proj_pages/firmware/firmware.zip
-zip ./proj_pages/firmware/firmware.zip ./build/target_firmware_/release/firmware.*
+rm -f .proj_pages/firmware/firmware.zip
+zip proj_pages/firmware/firmware.zip ./build/target_firmware_/release/firmware.*
 
 # Update test_result.xml
 cp -rf build/native_test_/debug/testreport/* project_pages/testreport
